384c45445eda308eab2c3ed6c645dd4c2220d196
mar10zmeji <mar10zmejia2007@gmail.com>
2026-04-06 16:25:33 -0700

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:18:17 2026
+
+@author: marpl
+"""
+
+a = ["Marlon Mejia", "any pronouns", "I do not have a favorite movie, ", "My favorite food MIGHT be bacon."]
+print(a[0])
+print(a[1])
+print(a[2] + a[3])
