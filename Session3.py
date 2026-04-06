235be5d8c61a6acaff8a3092f20a2066732ab8df
mar10zmeji <mar10zmejia2007@gmail.com>
2026-04-06 16:44:07 -0700

    Add files via upload

---
+++ Session3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:37:52 2026
+
+@author: marpl
+"""
+
+def f(x):
+    return x**3 + 8
+      
+def main():
+   print(f(9))
+   if f(9) >= 27 :
+       print("YIPPEE !!")
+            
+if __name__ == "__main__":
+    main()
