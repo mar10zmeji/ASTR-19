dbf0681b8c66e07ba642edbeba900540bcdee3ad
mar10zmeji <mar10zmejia2007@gmail.com>
2026-04-06 16:34:37 -0700

    Add files via upload

---
+++ Session2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:29:40 2026
+
+@author: marpl
+"""
+
+a = 25.8
+b = 67
+
+print(float(a)+float(b), type(float(a)+float(b)))
+print(int(a)-int(b), type(int(a)-int(b)))
+print(float(a)*int(b), type(float(a)*int(b)))
